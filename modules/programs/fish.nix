b4dbf91d48c6d353a87afedc48dac61c85a219aa
HigherOrderLogic <73709188+HigherOrderLogic@users.noreply.github.com>
2025-09-09 05:28:18 +0000

    feat(programs/fish): align keybinds with my zsh config

--- modules/programs/fish.nix
+++ modules/programs/fish.nix
@@ -9,7 +9,15 @@
 in {
   options.cfg.programs.fish.enable = mkEnableOption "fish";
   config = mkIf cfg.enable {
-    programs.fish.enable = true;
+    programs.fish = {
+      enable = true;
+      interactiveShellInit = ''
+        bind up history-prefix-search-backward
+        bind down history-prefix-search-forward
+        bind shift-up history-search-backward
+        bind shift-down history-search-forward
+      '';
+    };
     users.users.${config.cfg.core.username}.shell = pkgs.fish;
     environment.systemPackages = [pkgs.fishPlugins.hydro];
   };
