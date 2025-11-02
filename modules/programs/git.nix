8f6baca7e94124a4c07024c938b5cdfc2902cd82
HigherOrderLogic <73709188+HigherOrderLogic@users.noreply.github.com>
2025-11-02 06:55:08 +0000

    refactor(programs/git): dont install Git systemwide

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -5,6 +5,8 @@
   ...
 }: let
   cfg = config.cfg.programs.git;
+
+  gitIni = pkgs.formats.gitIni {};
 in {
   options.cfg.programs.git = {
     enable = lib.mkEnableOption "git";
@@ -18,18 +20,9 @@ in {
     };
   };
   config = lib.mkIf cfg.enable {
-    hj.packages = with pkgs; [gh codeberg-cli];
-    environment.shellAliases = {
-      gaa = "git add --all";
-      gcm = "git commit --message";
-      gca = "git commit --amend";
-      gcan = "git commit --amend --no-edit";
-      gp = "git push";
-      gpf = "git push --force-with-lease";
-    };
-    programs.git = {
-      enable = true;
-      config = {
+    hj = {
+      packages = with pkgs; [git gh codeberg-cli];
+      xdg.config.files."git/config".source = gitIni.generate "git-config" {
         user = {inherit (cfg) name email;};
         signing.format = "https";
         init.defaultBranch = "main";
@@ -38,6 +31,14 @@ in {
           "https://codeberg.org/".insteadOf = ["cb:" "codeberg:"];
         };
       };
+    };
+    environment.shellAliases = {
+      gaa = "git add --all";
+      gcm = "git commit --message";
+      gca = "git commit --amend";
+      gcan = "git commit --amend --no-edit";
+      gp = "git push";
+      gpf = "git push --force-with-lease";
     };
   };
 }
