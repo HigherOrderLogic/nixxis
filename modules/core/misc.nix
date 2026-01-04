b433cbaff7cd2063f28f20cc47a96e611f95b89c
HigherOrderLogic <73709188+HigherOrderLogic@users.noreply.github.com>
2026-01-04 15:52:29 +0000

    chore(core/misc): remove useless thing

--- modules/core/misc.nix
+++ modules/core/misc.nix
@@ -1,8 +1,10 @@
-{
+{lib, ...}: {
   config = {
     boot.tmp = {
       useTmpfs = true;
       tmpfsSize = "50%";
     };
+    programs.command-not-found.enable = false;
+    environment.defaultPackages = lib.mkDefault [];
   };
 }
