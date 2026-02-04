51b7821ee05bbfd03ee89411d556dadad8150054
HigherOrderLogic <73709188+HigherOrderLogic@users.noreply.github.com>
2026-02-04 22:52:06 +0700

    fix(desktops/plasma6): disable orca

--- modules/desktops/plasma6.nix
+++ modules/desktops/plasma6.nix
@@ -8,9 +8,12 @@
 in {
   options.cfg.desktops.plasma6.enable = lib.mkEnableOption "Plasma 6";
   config = lib.mkIf cfg.enable {
-    services.desktopManager.plasma6 = {
-      enable = true;
-      notoPackage = pkgs.nerd-fonts.noto;
+    services = {
+      desktopManager.plasma6 = {
+        enable = true;
+        notoPackage = pkgs.nerd-fonts.noto;
+      };
+      orca.enable = false;
     };
     environment.plasma6.excludePackages = with pkgs.kdePackages; [kwin-x11 ark konsole kate ktexteditor];
     hj.xdg.config.files."plasma-workspace/env/variables.sh".source = config.hj.environment.loadEnv;
