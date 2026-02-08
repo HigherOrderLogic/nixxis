ddde0a2951db5f9581d8d46a993b8138be953a0c
HigherOrderLogic <73709188+HigherOrderLogic@users.noreply.github.com>
2026-02-08 03:35:31 +0000

    chore(core/misc): who need docs

--- modules/core/misc.nix
+++ modules/core/misc.nix
@@ -5,6 +5,8 @@
       tmpfsSize = "50%";
     };
     programs.command-not-found.enable = false;
+    documentation.info.enable = false;
+    documentation.nixos.enable = false;
     environment.defaultPackages = lib.mkDefault [];
   };
 }
