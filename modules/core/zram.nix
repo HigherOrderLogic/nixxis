a87fe2ca341205486e406c5da97e441fc9ff509a
HigherOrderLogic <73709188+HigherOrderLogic@users.noreply.github.com>
2026-03-31 18:48:20 +1100

    fix(core/zram): tweak sysctl params to avoid freezing system

--- modules/core/zram.nix
+++ modules/core/zram.nix
@@ -12,6 +12,8 @@ in {
       "vm.watermark_boost_factor" = 0;
       "vm.watermark_scale_factor" = 125;
       "vm.page-cluster" = 0;
+      "vm.overcommit_memory" = 1;
+      "vm.overcommit_ratio" = 95;
     };
     zramSwap.enable = true;
   };
