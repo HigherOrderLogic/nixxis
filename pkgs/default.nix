051a1dcab266dac768ef5ac6c0e2284a5ef500f6
HigherOrderLogic <73709188+HigherOrderLogic@users.noreply.github.com>
2026-04-07 07:59:14 +0000

    refactor(pkgs): simplify path concatenation

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -8,7 +8,7 @@
       localPackages = lib.pipe ./. [
         builtins.readDir
         (lib.filterAttrs (_: v: v == "directory"))
-        (builtins.mapAttrs (k: _: self.callPackage "${./.}/${k}" {inherit pins;}))
+        (builtins.mapAttrs (k: _: self.callPackage ./${k} {inherit pins;}))
       ];
     })
   ];
