547893a9cffd9ccf9a2f876d07c55cf926ac39c2
HigherOrderLogic <73709188+HigherOrderLogic@users.noreply.github.com>
2026-05-02 22:17:09 +1000

    chore: switch back to upstream `nix-flatpak`

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
         nix-darwin.follows = "";
       };
     };
-    nix-flatpak.url = "github:higherorderlogic/nfp";
+    nix-flatpak.url = "github:gmodena/nix-flatpak";
     nixos-wsl = {
       url = "github:nix-community/nixos-wsl";
       inputs = {
