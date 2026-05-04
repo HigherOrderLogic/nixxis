b1999412a1d56c794232c5358207718879359f77
HigherOrderLogic <73709188+HigherOrderLogic@users.noreply.github.com>
2026-05-04 22:38:50 +1000

    refactor(pkgs/niri-git): refactor

--- pkgs/niri-git/default.nix
+++ pkgs/niri-git/default.nix
@@ -5,13 +5,12 @@
 }:
 niri.overrideAttrs (final: prev: {
   pname = "${prev.pname}-git";
-  version = "26.04";
   src = pins.niri;
-  postPatch = ''
-    patchShebangs resources/niri-session
-    substituteInPlace resources/niri.service --replace-fail 'ExecStart=niri' "ExecStart=$out/bin/niri"
-    substituteInPlace resources/niri.desktop --replace-fail 'Exec=niri-session' "Exec=$out/bin/niri-session"
-  '';
+  postPatch =
+    prev.postPatch
+    + ''
+      substituteInPlace resources/niri.desktop --replace-fail 'Exec=niri-session' "Exec=$out/bin/niri-session"
+    '';
   cargoDeps = rustPlatform.fetchCargoVendor {
     inherit (final) src;
     hash = "sha256-gfnalA3qI3a9h3PvsxgQLCrzapfjLLkxhTMJpwRh+ro=";
