2b7352cf7af31549634a5fb1c235d96a8b17f6b3
HigherOrderLogic <73709188+HigherOrderLogic@users.noreply.github.com>
2026-05-27 11:23:00 +1000

    chore(core/misc): eradicate the perls

--- modules/core/misc.nix
+++ modules/core/misc.nix
@@ -4,9 +4,12 @@
       useTmpfs = true;
       tmpfsSize = "50%";
     };
+    system.tools.nixos-generate-config.enable = lib.mkDefault false;
     programs.command-not-found.enable = false;
-    documentation.info.enable = false;
-    documentation.nixos.enable = false;
+    documentation = {
+      info.enable = false;
+      nixos.enable = false;
+    };
     environment.defaultPackages = lib.mkDefault [];
   };
 }
