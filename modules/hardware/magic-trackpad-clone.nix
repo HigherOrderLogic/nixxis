4341b32f649713c5fd178e4d74c91c3563ae5a8c
HigherOrderLogic <73709188+HigherOrderLogic@users.noreply.github.com>
2026-07-01 08:07:14 +0000

    refactor: remove `getExe` where possible

--- modules/hardware/magic-trackpad-clone.nix
+++ modules/hardware/magic-trackpad-clone.nix
@@ -9,12 +9,12 @@ in {
   options.cfg.hardware.magic-trackpad-clone.enable = lib.mkEnableOption "Apple's Magic Trackpad clone";
   config = lib.mkIf cfg.enable {
     services.udev.extraRules = let
-      switchDriver = pkgs.writeShellScriptBin "switch-driver" ''
+      switchDriver = pkgs.writeShellScript "switch-driver" ''
         echo -n $1 > /sys/bus/hid/drivers/magicmouse/unbind
         echo -n $1 > /sys/bus/hid/drivers/hid-multitouch/bind
       '';
     in ''
-      SUBSYSTEM=="hid", ATTRS{idVendor}=="05ac", ATTRS{idProduct}=="0265", PROGRAM="${lib.getExe switchDriver} %k"
+      SUBSYSTEM=="hid", ATTRS{idVendor}=="05ac", ATTRS{idProduct}=="0265", PROGRAM="${switchDriver} %k"
     '';
   };
 }
