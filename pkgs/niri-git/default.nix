0b46cc2dc2788d4503bc804db463aea8458c9f95
HigherOrderLogic <73709188+HigherOrderLogic@users.noreply.github.com>
2026-07-23 01:52:37 +1000

    feat(pkgs/niri-git): no more manual hash

--- pkgs/niri-git/default.nix
+++ pkgs/niri-git/default.nix
@@ -11,14 +11,9 @@ niri.overrideAttrs (final: prev: {
     + ''
       substituteInPlace resources/niri.desktop --replace-fail 'Exec=niri-session' "Exec=$out/bin/niri-session"
     '';
-  cargoDeps = rustPlatform.fetchCargoVendor {
-    inherit (final) src;
-    hash = "sha256-gfnalA3qI3a9h3PvsxgQLCrzapfjLLkxhTMJpwRh+ro=";
+  cargoDeps = rustPlatform.importCargoLock {
+    lockFile = "${final.src}/Cargo.lock";
+    allowBuiltinFetchGit = true;
   };
-  env =
-    prev.env
-    // {
-      RUSTFLAGS = "${prev.env.RUSTFLAGS} -C target-cpu=native";
-      NIRI_BUILD_COMMIT = final.src.revision;
-    };
+  env = prev.env // {NIRI_BUILD_COMMIT = final.src.revision;};
 })
