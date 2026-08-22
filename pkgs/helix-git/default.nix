fab2779fd5b07d71b9bd3c28fefafecadd94596f
HigherOrderLogic <73709188+HigherOrderLogic@users.noreply.github.com>
2026-08-23 03:32:30 +1000

    fix(pkgs/helix-git): alias begone

--- pkgs/helix-git/default.nix
+++ pkgs/helix-git/default.nix
@@ -1,7 +1,13 @@
 {
   pins,
+  runCommand,
   callPackage,
 }: let
   pin = pins.helix;
+  pin' = runCommand "helix-pin" {} ''
+    cp -r ${pin} $out
+
+    substituteInPlace $out/grammars.nix --replace-fail 'stdenv.isLinux' 'stdenv.hostPlatform.isLinux'
+  '';
 in
-  callPackage "${pin}/default.nix" {gitRev = pin.revision;}
+  callPackage "${pin'}/default.nix" {gitRev = pin.revision;}
