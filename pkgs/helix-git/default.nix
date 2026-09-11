53d11c011915066d12c973fe871be384431348b6
HigherOrderLogic <73709188+HigherOrderLogic@users.noreply.github.com>
2026-09-11 21:49:40 +1000

    fix(pkgs/helix-git): switch to fork
    no more ifd

--- pkgs/helix-git/default.nix
+++ pkgs/helix-git/default.nix
@@ -1,13 +1,7 @@
 {
   pins,
-  runCommand,
   callPackage,
 }: let
   pin = pins.helix;
-  pin' = runCommand "helix-pin" {} ''
-    cp -r ${pin} $out
-
-    substituteInPlace $out/grammars.nix --replace-fail 'stdenv.isLinux' 'stdenv.hostPlatform.isLinux'
-  '';
 in
-  callPackage "${pin'}/default.nix" {gitRev = pin.revision;}
+  callPackage "${pin}/default.nix" {gitRev = pin.revision;}
